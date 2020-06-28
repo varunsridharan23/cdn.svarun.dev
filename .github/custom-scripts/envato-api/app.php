44cfe9147889edcdefc558b26fde3ad8d47aca4c
Varun <varunsridharan23@gmail.com>
2020-06-28 12:20:20 +0530

    Added Docs & Github Docs Link

--- .github/custom-scripts/envato-api/app.php
+++ .github/custom-scripts/envato-api/app.php
@@ -35,6 +35,8 @@ try {
 
 			$data['slug']      = strtolower( preg_replace( '~[^\pL\d]+~u', '-', $item->name ) );
 			$data['mini_slug'] = slugify( $data['slug'] );
+			$data['docs']      = 'https://plugin-docs.varunsridharan.in/' . $data['slug'];
+			$data['docs-git']  = 'https://github.com/vs-docs/' . $data['slug'];
 
 			if ( isset( $item->previews->icon_with_video_preview ) ) {
 				$data['banner'] = $item->previews->icon_with_video_preview->landscape_url;
